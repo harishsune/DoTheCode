d1c46b69091e2a632c1561042c169f0c746d5b19
harishsune <harishsune@hotmail.com>
2016-07-14 23:19:17 +0530

    Initial check in for ChangeMyPassword

---
+++ Product/ChangeMyPassword/PasswordManager.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ChangeMyPassword
+{
+    partial class PasswordManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasswordManager));
+            this.txtOldPassword = new System.Windows.Forms.TextBox();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.BtnChangePassword = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblWelcome = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtOldPassword
+            // 
+            this.txtOldPassword.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOldPassword.Location = new System.Drawing.Point(262, 50);
+            this.txtOldPassword.Name = "txtOldPassword";
+            this.txtOldPassword.PasswordChar = '*';
+            this.txtOldPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtOldPassword.TabIndex = 0;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewPassword.Location = new System.Drawing.Point(262, 85);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.PasswordChar = '*';
+            this.txtNewPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtNewPassword.TabIndex = 1;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(262, 124);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.PasswordChar = '*';
+            this.txtConfirmPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtConfirmPassword.TabIndex = 2;
+            // 
+            // BtnChangePassword
+            // 
+            this.BtnChangePassword.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnChangePassword.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnChangePassword.Location = new System.Drawing.Point(158, 176);
+            this.BtnChangePassword.Name = "BtnChangePassword";
+            this.BtnChangePassword.Size = new System.Drawing.Size(118, 23);
+            this.BtnChangePassword.TabIndex = 3;
+            this.BtnChangePassword.Text = "Change Password";
+            this.BtnChangePassword.UseVisualStyleBackColor = true;
+            this.BtnChangePassword.Click += new System.EventHandler(this.BtnChangePassword_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnReset.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.Location = new System.Drawing.Point(299, 176);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 4;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Old Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "New Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Confirm Password";
+            // 
+            // LblWelcome
+            // 
+            this.LblWelcome.AutoSize = true;
+            this.LblWelcome.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblWelcome.Location = new System.Drawing.Point(12, 9);
+            this.LblWelcome.Name = "LblWelcome";
+            this.LblWelcome.Size = new System.Drawing.Size(25, 15);
+            this.LblWelcome.TabIndex = 8;
+            this.LblWelcome.Text = "Hi, ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChangeMyPassword.Properties.Resources.change_password;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PasswordManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 226);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LblWelcome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnChangePassword);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.txtOldPassword);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PasswordManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change My Password";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOldPassword;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Button BtnChangePassword;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblWelcome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Product/ChangeMyPassword/PasswordManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+using System.IO;
+
+namespace ChangeMyPassword
+{
+    public partial class PasswordManager : Form
+    {
+        public PasswordManager()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChangePassword_Click(object sender, EventArgs e)
+        {
+            if (txtNewPassword.Text.Equals(txtConfirmPassword.Text))
+            { 
+                string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+                var Context = new PrincipalContext(ContextType.Machine);
+            var User = UserPrincipal.FindByIdentity(Context, IdentityType.SamAccountName, userName);
+                try
+                {
+                    User.ChangePassword(txtOldPassword.Text, txtNewPassword.Text);
+                    MessageBox.Show("Password Changed successfully");
+                    CreateLog();
+                }
+                catch(Exception ex)
+                {
+                    if (ex.InnerException is InvalidOperationException)
+                    {
+                        MessageBox.Show("Please enter correct old password");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please start application with administrative access to generate log");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Password doesnot match");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LblWelcome.Text += System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtConfirmPassword.Text = string.Empty;
+            txtNewPassword.Text = string.Empty;
+            txtOldPassword.Text = string.Empty;
+        }
+
+        private void CreateLog()
+        {
+            string sFilePath = AppDomain.CurrentDomain.BaseDirectory + "Change My Password Log.txt";
+            StreamWriter oStramWriter = new StreamWriter(sFilePath,true);
+            string sFileData = "Your Password is changed on: " + DateTime.Now + " , Old Password: " + txtOldPassword.Text + " , New Password: " + txtNewPassword.Text;
+            oStramWriter.WriteLine(sFileData);
+            oStramWriter.Close();
+
+        }
+    }
+}
